5b3357c86f03191d62b69a96cacc8036f6a20748
Luis Capelo <luiscape@gmail.com>
2016-04-17 14:29:58 -0400

    Adding jupyterhub configuration file.'

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -14,16 +14,16 @@ is safely stored in disk.
 
 c = get_config()
 
-c.Authenticator.admin_users = {'luiscapelo'}
-
-c.JupyterHub.port = 80
-c.JupyterHub.hub_port = 8001
-
+c.JupyterHub.port = 8000
 c.JupyterHub.hub_ip = '0.0.0.0'
-c.JupyterHub.proxy_api_ip = '0.0.0.0'
-c.DockerSpawner.hub_ip_connect = '172.17.0.1'
-
-c.Spawner.debug = True
-c.PAMAuthenticator.create_system_users = True
-c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
+c.Authenticator.admin_users = {'luiscapelo'}
 c.JupyterHub.cookie_secret_file = '/root/jupyterhub_cookie_secret'
+
+# c.JupyterHub.hub_port = 8001
+
+# c.JupyterHub.proxy_api_ip = '0.0.0.0'
+# c.DockerSpawner.hub_ip_connect = '172.17.0.1'  # docker0 insterface address.
+
+# c.Spawner.debug = True
+# c.PAMAuthenticator.create_system_users = True
+# c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
